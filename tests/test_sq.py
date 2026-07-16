9c6b0da81feed6782349a600ad85b426eb94d9bd
Ruth Shangobiyi <rshangobiyi@microsoft.com>
2026-07-16 16:16:00 +0100

    Adds more failing tests

--- tests/test_sq.py
+++ tests/test_sq.py
@@ -8,6 +8,8 @@ class SmokeTest(unittest.TestCase):
 class TestSquare(unittest.TestCase):
     def test_root_of0(self):
         self.assertEqual(sq_num(0), 0)
+        self.assertEqual(sq_num(4), 2)
+        self.assertEqual(sq_num(9), 3)
 
 if __name__ == '__main__':
     unittest.main()
