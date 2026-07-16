e8e6c722e6034ab57d54dd150443584516f737ac
Ruth Shangobiyi <rshangobiyi@microsoft.com>
2026-07-16 16:10:09 +0100

    Added failing test

--- tests/test_sq.py
+++ tests/test_sq.py
@@ -1,8 +1,13 @@
 import unittest
+from root import sq_num
 
 class SmokeTest(unittest.TestCase):
     def test2_and2(self):
         self.assertEqual(2 + 2, 4)
+
+class TestSquare(unittest.TestCase):
+    def test_root_of0(self):
+        self.assertEqual(sq_num(0), 0)
 
 if __name__ == '__main__':
     unittest.main()
